84adce6881059d438943d1337ab6de9abd3fb70c
khalifahnur <khalifahnur1095@gmail.com>
2023-12-14 07:12:19 -0800

    pop

---
+++ pop_1.c
@@ -0,0 +1,23 @@
+#include "monty_props.h"
+/**
+ * func_pop - removes the top element of the stack
+ * @counter: counter
+ * @head: stack head
+ * Return: no null(0)
+*/
+void func_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
